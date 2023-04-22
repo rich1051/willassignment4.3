4773eb0cbb204ebabffe48e4edc245f5605859f6
Will Richardson <rich1051@umn.edu>
2023-04-22 09:49:48 -0500

    commit

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,20 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+function addItem(){
+    if (basket.length <= 5){
+        basket.push('lava lamp');
+        return true;
+    }
+    else{
+        return 'I think I have enough.';
+    }
+}
+console.log(`Basket is ${basket}`);
+console.log('Adding lava lamp (expect true):', addItem('lava lamp'));
+console.log(`Basket is now ${basket}`);
+
+function listItems(){
+
+}
